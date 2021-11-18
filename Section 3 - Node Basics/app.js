dea6b4688ccb8bd64ea60eef0b519c1b25c44b01
adclleva <arvin.lleva@yahoo.com>
2021-11-17 22:12:03 -0800

    Looking at requests

--- Section 3 - Node Basics/app.js
+++ Section 3 - Node Basics/app.js
@@ -3,7 +3,12 @@ const http = require("http");
 
 // server callback function
 const server = http.createServer((req, res) => {
-  console.log("req", req);
+  // console.log("req", req);
+  console.log("url", req.url);
+  console.log("method", req.method);
+  console.log("headers", req.headers);
+  // this exits the event loop of the server and kills the server
+  // process.exit();
 });
 
 server.listen(3000);
