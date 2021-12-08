2f223ff668c1824124317e94bb43686bed7bff30
adclleva <arvin.lleva@yahoo.com>
2021-12-07 20:43:33 -0800

    adding middleware

--- Section 5 - Working with Express.js/app.js
+++ Section 5 - Working with Express.js/app.js
@@ -3,10 +3,21 @@
 // http is a global module
 const http = require("http");
 
+// express.js is all about middleware
 const express = require("express");
 
 const app = express();
 
+// middle ware function
+// next is a function
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("Another the middleware!");
+});
 // server callback function
 const server = http.createServer(app);
 
