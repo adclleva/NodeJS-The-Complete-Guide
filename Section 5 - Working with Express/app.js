a56eea4635561b2b4ac42a99ab7910d9ca7c92a0
adclleva <arvin.lleva@yahoo.com>
2021-12-09 18:43:11 -0800

    Returning a 404 page

--- Section 5 - Working with Express/app.js
+++ Section 5 - Working with Express/app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 // this file just spins up the server
 
 // express.js is all about middleware, that executes from top to bottom
@@ -24,7 +26,8 @@ app.use(shopRoutes);
 
 // a catch all route when the url doesn't match anything
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  // res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
