d428bda78dc38efbb625992dfc9885ddea01cb45
adclleva <arvin.lleva@yahoo.com>
2021-12-09 21:29:31 -0800

    Installing and implementing Pug

--- Section 6 - Working with Dynamic Content and Adding Templating Engines/app.js
+++ Section 6 - Working with Dynamic Content and Adding Templating Engines/app.js
@@ -1,21 +1,27 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// uses express to compile dynamic templates with pug engine
+app.set("view engine", "pug");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+// to show us where to find the templates
+app.set("views", "views");
 
-app.use('/admin', adminData.routes);
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- Section 6 - Working with Dynamic Content and Adding Templating Engines/routes/shop.js
+++ Section 6 - Working with Dynamic Content and Adding Templating Engines/routes/shop.js
@@ -1,15 +1,19 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get("/", (req, res, next) => {
+  // console.log('shop.js', adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+
+  // this will use the default template engine declared in the app.js file
+  // we don't have to construct a path to the folder since we defined that all views are in the views folder
+  res.render("shop");
 });
 
 module.exports = router;
