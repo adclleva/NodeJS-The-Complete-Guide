2722df4b06abf78196efdcd171a1e33e2b3c4798
adclleva <arvin.lleva@yahoo.com>
2021-12-09 18:34:37 -0800

    Serving HTML Pages

--- Section 5 - Working with Express/routes/admin.js
+++ Section 5 - Working with Express/routes/admin.js
@@ -1,18 +1,22 @@
 const express = require("express");
 
+const path = require("path");
+
 const router = express.Router();
 
 // this will only trigger for only post requests
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    `<form action="/admin/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button> </form>`
-  );
+  // res.send(
+  //   `<form action="/admin/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button> </form>`
+  // );
+
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 // this will only trigger for only post requests
 // /admin/add-product => POST
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log("request:", req.body);
   res.redirect("/");
 });
---
+++ Section 5 - Working with Express/routes/shop.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const path = require("path");
+
+const router = express.Router();
+
+// get uses an exact match
+router.get(`/`, (req, res, next) => {
+  // console.log("Shop Route is hit");
+  // res.send("<h1>Hello from Express!</h1>");
+
+  // __dirname holds the absolute path to this project folder
+  // __dirname also starts/points to where it is being used, in this case the routes folder
+  // we do ../ to go up one level to the views
+  // we do path.join() because we want this to work on different OS systems
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+});
+
+module.exports = router;
