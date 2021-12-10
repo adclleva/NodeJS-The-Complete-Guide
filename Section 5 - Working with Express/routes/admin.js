24fc75afbc78a9047944d917023874eda9014cff
adclleva <arvin.lleva@yahoo.com>
2021-12-09 18:49:51 -0800

    Using a helper function for Navigation

--- Section 5 - Working with Express/routes/admin.js
+++ Section 5 - Working with Express/routes/admin.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const path = require("path");
 
+const rootDir = require("../utils/path");
+
 const router = express.Router();
 
 // this will only trigger for only post requests
@@ -11,7 +13,7 @@ router.get("/add-product", (req, res, next) => {
   //   `<form action="/admin/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button> </form>`
   // );
 
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // this will only trigger for only post requests
--- Section 5 - Working with Express/routes/shop.js
+++ Section 5 - Working with Express/routes/shop.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const path = require("path");
 
+const rootDir = require("../utils/path");
+
 const router = express.Router();
 
 // get uses an exact match
@@ -13,7 +15,7 @@ router.get(`/`, (req, res, next) => {
   // __dirname also starts/points to where it is being used, in this case the routes folder
   // we do ../ to go up one level to the views
   // we do path.join() because we want this to work on different OS systems
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
