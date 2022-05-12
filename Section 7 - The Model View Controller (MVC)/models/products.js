936311c4c4e1a43cf500adc9677b68ad0f32d2aa
adclleva <arvin.lleva@yahoo.com>
2022-05-11 22:43:21 -0700

    store data in files via the Model

--- Section 7 - The Model View Controller (MVC)/models/products.js
+++ Section 7 - The Model View Controller (MVC)/models/products.js
@@ -1,15 +1,44 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
+const rootDir = require("../util/path");
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    products.push(this);
+    const p = path.join(rootDir, "data", "products.json");
+
+    // reads the entire files content of a file
+    // since we are using an arrow function, 'this' would be referred to this class
+    fs.readFile(p, (err, filesContent) => {
+      // console.log(filesContent);
+      let products = [];
+
+      if (!err) {
+        // our filesContent will be a JSON and we need to parse it
+        products = JSON.parse(filesContent);
+      }
+
+      products.push(this);
+
+      // to save to the file
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    const p = path.join(rootDir, "data", "products.json");
+
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+
+      return JSON.parse(fileContent);
+    });
   }
 };
