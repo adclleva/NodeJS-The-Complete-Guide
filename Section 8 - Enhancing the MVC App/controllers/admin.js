95e72097ef36d5833a084c06f87982bd8e081673
adclleva <arvin.lleva@yahoo.com>
2022-05-12 23:56:59 -0700

    registering the route files

--- Section 8 - Enhancing the MVC App/controllers/admin.js
+++ Section 8 - Enhancing the MVC App/controllers/admin.js
@@ -5,9 +5,6 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
   });
 };
 
@@ -20,13 +17,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render("shop/product-list", {
+    res.render("admin/products", {
       products: products,
-      pageTitle: "My Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
     });
   });
 };
