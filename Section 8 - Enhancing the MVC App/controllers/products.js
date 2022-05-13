c2e30b36d05646c07f850e6ba374dc3fe0f3b91c
adclleva <arvin.lleva@yahoo.com>
2022-05-12 23:03:53 -0700

    created the shop structure

--- Section 8 - Enhancing the MVC App/controllers/products.js
+++ Section 8 - Enhancing the MVC App/controllers/products.js
@@ -2,7 +2,7 @@ const Product = require("../models/products");
 
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formsCSS: true,
@@ -20,7 +20,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       products: products,
       pageTitle: "My Shop",
       path: "/",
