c94710a77b356942938bb8f4365fdfce25e7941a
adclleva <arvin.lleva@yahoo.com>
2022-05-18 12:24:33 -0700

    passing data with post request

--- Section 9 - Dynamic Routes & Advanced Models/controllers/shop.js
+++ Section 9 - Dynamic Routes & Advanced Models/controllers/shop.js
@@ -39,6 +39,12 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+  console.log("productId", productId);
+  res.redirect("/cart");
+};
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "/orders",
