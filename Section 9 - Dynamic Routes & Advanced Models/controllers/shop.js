4f61e9c96625cbd4e9650f52a051029991372afe
adclleva <arvin.lleva@yahoo.com>
2022-05-18 16:38:07 -0700

    adding a cart model

--- Section 9 - Dynamic Routes & Advanced Models/controllers/shop.js
+++ Section 9 - Dynamic Routes & Advanced Models/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -41,7 +42,12 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-  console.log("productId", productId);
+  Product.findById(productId, (product) => {
+    console.log("productId", productId);
+    console.log("product", product);
+    Cart.addProduct(productId, product.price);
+  });
+
   res.redirect("/cart");
 };
 
