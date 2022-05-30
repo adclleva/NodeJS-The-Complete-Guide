acda9fd270c472427779012ebd9d172d10091e12
adclleva <arvin.lleva@yahoo.com>
2022-05-29 18:41:55 -0700

    defined the product sequailzed modal

--- Section 11 - Understanding Sequelize/util/database.js
+++ Section 11 - Understanding Sequelize/util/database.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 
+// this is the entire database collection pool package
 const sequelize = new Sequelize("node-complete", "root", "mysql123!", {
   dialect: "mysql",
   host: "localhost",
