9f07a8f9bd7c78cce525bda28567887534d735a0
adclleva <arvin.lleva@yahoo.com>
2022-05-30 15:22:44 -0700

    deleting connected items and cart items

--- Section 11 - Understanding Sequelize/controllers/shop.js
+++ Section 11 - Understanding Sequelize/controllers/shop.js
@@ -113,10 +113,20 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const productId = req.body.productId;
-  Product.findByPk(productId, (product) => {
-    Cart.deleteProduct(productId, product.price);
-    res.redirect("/cart");
-  });
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts({ where: { id: productId } });
+    })
+    .then((products) => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then((result) => {
+      console.log("Cart Destroyed!");
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
