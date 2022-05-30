7809ada181ac8191a50ce2c39c53e10d89c7f6a0
adclleva <arvin.lleva@yahoo.com>
2022-05-30 16:14:35 -0700

    able to reset the cart and also outputting orders

--- Section 11 - Understanding Sequelize/controllers/shop.js
+++ Section 11 - Understanding Sequelize/controllers/shop.js
@@ -128,9 +128,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -152,18 +154,30 @@ exports.postOrder = (req, res, next) => {
         .catch((err) => console.log(err));
     })
     .then((result) => {
+      // this will clean up the cart after an order has been set
+      return fetchedCart.setProducts(null);
+    })
+    .then((result) => {
       res.redirect("/orders");
     })
     .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
+  req.user
+    // with the concept of eager loading through sequelize which makes this work because of the relationships
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
+// currently not being used
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     path: "/checkout",
