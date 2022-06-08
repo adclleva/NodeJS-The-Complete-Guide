61d4a1a5af53edb1b8f1d2ff525e85cce623b761
adclleva <arvin.lleva@yahoo.com>
2022-06-08 16:35:40 -0700

    clearing cart after storing order

--- Section 13 - Working with Mongoose/models/user.js
+++ Section 13 - Working with Mongoose/models/user.js
@@ -64,6 +64,13 @@ userSchema.methods.removeFromCart = function (productId) {
   return this.save(); // we utilize mongoose's static methods
 };
 
+userSchema.methods.clearCart = function () {
+  this.cart = {
+    items: [],
+  };
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
 
 // const mongodb = require("mongodb");
