d0c54c58acf4c48f3b315cff7c56a8a8e7189148
adclleva <arvin.lleva@yahoo.com>
2022-06-11 00:16:44 -0700

    started on the reset password logic

--- Section 17 - Advanced Authentication/controllers/auth.js
+++ Section 17 - Advanced Authentication/controllers/auth.js
@@ -135,3 +135,19 @@ exports.postLogout = (req, res, next) => {
     res.redirect("/");
   });
 };
+
+exports.getReset = (req, res, next) => {
+  let errorMessage = req.flash("error");
+
+  if (errorMessage.length > 0) {
+    errorMessage = errorMessage[0];
+  } else {
+    errorMessage = null;
+  }
+
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: errorMessage,
+  });
+};
