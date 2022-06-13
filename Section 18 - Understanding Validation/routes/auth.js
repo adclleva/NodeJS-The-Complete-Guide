7439466f22f9dfe2c66e89a72a79b3a1515b0e6b
adclleva <arvin.lleva@yahoo.com>
2022-06-12 17:56:57 -0700

    checking for filed validity

--- Section 18 - Understanding Validation/routes/auth.js
+++ Section 18 - Understanding Validation/routes/auth.js
@@ -24,13 +24,23 @@ router.post(
         if (value === "test@test.com") {
           throw new Error("This email triggers our custom validation");
         }
+        // have to return true for the validations to pass
         return true;
       }),
+
     // this will validation the req body
     // adding the message to the second parameter displays the message for all validation error checks
     body("password", "Please enter a password with only numbers and text and at least 5 characters")
       .isLength({ min: 5 })
       .isAlphanumeric(),
+
+    // this validation checks if the password and confirmPassword is the same
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password have to match!");
+      }
+      return true;
+    }),
   ],
   authController.postSignup
 );
