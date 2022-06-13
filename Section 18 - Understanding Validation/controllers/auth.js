3614ecaa04fedd1ff471693fc0d5507d0286ce37
adclleva <arvin.lleva@yahoo.com>
2022-06-12 18:57:49 -0700

    added validation for logging in

--- Section 18 - Understanding Validation/controllers/auth.js
+++ Section 18 - Understanding Validation/controllers/auth.js
@@ -46,6 +46,16 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("auth/login", {
+      path: "/login",
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
 
   User.findOne({ email: email })
     .then((user) => {
@@ -90,7 +100,7 @@ exports.postSignup = (req, res, next) => {
   if (!errors.isEmpty()) {
     console.log(errors.array());
     return res.status(422).render("auth/signup", {
-      path: "signup",
+      path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
     });
