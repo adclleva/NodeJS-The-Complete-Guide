9941b554f3ad9b4e42911dafad3151453d297213
adclleva <arvin.lleva@yahoo.com>
2022-06-12 17:41:02 -0700

    built in custom validation

--- Section 18 - Understanding Validation/routes/auth.js
+++ Section 18 - Understanding Validation/routes/auth.js
@@ -14,7 +14,19 @@ router.get("/signup", authController.getSignup);
 router.post("/login", authController.postLogin);
 
 // email validation
-router.post("/signup", check("email").isEmail().withMessage("Please enter a valid email"), authController.postSignup);
+router.post(
+  "/signup",
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .custom((value, { req }) => {
+      if (value === "test@test.com") {
+        throw new Error("This email triggers our custom validation");
+      }
+      return true;
+    }),
+  authController.postSignup
+);
 
 router.post("/logout", authController.postLogout);
 
