c992fccce751dd31f272070c30b02f74d1564a33
adclleva <arvin.lleva@yahoo.com>
2022-06-13 17:39:28 -0700

    added comments

--- Section 19 - Error Handling/app.js
+++ Section 19 - Error Handling/app.js
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then((user) => {
-      // throw new Error("Dummy async error");
+      // throw new Error("Dummy async error"); <- to test async errors
       if (!user) {
         return next();
       }
