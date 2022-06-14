c814ccb67266d001c6cecf846a87000602230493
adclleva <arvin.lleva@yahoo.com>
2022-06-14 00:25:18 -0700

    re-using the pagination logic & controls

--- Section 21 - Adding Pagination/controllers/shop.js
+++ Section 21 - Adding Pagination/controllers/shop.js
@@ -9,13 +9,31 @@ const Order = require("../models/order");
 const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
+  // we set the query param within the view and converted to a num
+  const page = +req.query.page || 1; // if query param is undefined, it defaults to 1
+
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then((productsCount) => {
+      totalItems = productsCount;
+
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE) // skips the items of the previous pages
+        .limit(ITEMS_PER_PAGE); // limits the amount of data presented
+    })
     .then((products) => {
-      console.log(products);
       res.render("shop/product-list", {
         prods: products,
-        pageTitle: "All Products",
+        pageTitle: "Products",
         path: "/products",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems, //  next page logic
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
